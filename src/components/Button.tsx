9da92866d20c6a4952a0b86a2170cc4e5ae4232c
Birasa Divine <b.divine@alustudent.com>
2025-05-04 15:30:18 +0200

    fix(Button):ES6 syntax on sumOfTwo and added export default on the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,9 @@
-export function Button() {
-    function sumOfTwo(num1: number, num2: number): void {
-      const sum = num1 + num2;
-      alert(`The sum of ${num1} and ${num2} is ${sum}`);
-    }
-  
-    return (
-      <button onClick={() => sumOfTwo(5, 3)}>
-        Click me
-      </button>
-    );
-  }
+export function Button(){
+  const sumOfTwo = (num1:number,num2:number):void => {alert(`The sum of ${num1} and ${num2} is ${num1+num2}`)}  
+  return(
+     <button onClick={() => sumOfTwo(5,4) }> Click me </button>
+  )
+ 
+}
+
+export default Button
