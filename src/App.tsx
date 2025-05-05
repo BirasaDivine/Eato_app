466155ec85285b6154183e46d6e216341da25f3c
Birasa Divine <b.divine@alustudent.com>
2025-05-06 00:36:51 +0200

    fix(Button):ES6 syntax on sumOfTwo and added export default on the button

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Button} from "./components/Button"
-
+import Button from './components/Button'
+import React from 'react'
 function App() {
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" rel='noreferrer' target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" rel='noreferrer' target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,6 @@
-export function Button(){
-  const sumOfTwo = (num1:number,num2:number):void => {alert(`The sum of ${num1} and ${num2} is ${num1+num2}`)}  
-  return(
-     <button onClick={() => sumOfTwo(5,4) }> Click me </button>
-  )
- 
-}
-
-export default Button
+import React from 'react';
+const Button = () => {
+  const sumOfTwo = (num1:number, num2:number) => alert(`The sum of ${num1} and ${num2} is ${num1+num2}`) 
+   return (<button onClick={() => sumOfTwo(5,4) }> Click me </button>)
+ }
+ export default Button
