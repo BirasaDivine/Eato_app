61229b0b5427bba28336a3ec54c58e6c94ec32cd
Birasa Divine <b.divine@alustudent.com>
2025-05-07 15:08:43 +0200

    fix(prettier):Adding data in the config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from './components/Button'
-import React from 'react'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "./components/Button";
+import React from "react";
 function App() {
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" rel='noreferrer' target="_blank">
+        <a href="https://vite.dev" rel="noreferrer" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" rel='noreferrer' target="_blank">
+        <a href="https://react.dev" rel="noreferrer" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -27,7 +25,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
