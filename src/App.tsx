5dca3ba81ddf7e974f26c625287229f24fa1fd72
Birasa Divine <b.divine@alustudent.com>
2025-05-07 15:18:36 +0200

    fix(vercel):Fix React Import

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Button from "./components/Button";
-import React from "react";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Button from './components/Button';
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Eato App </h1>
-      <div className="card">
-        <Button />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<a href="https://vite.dev" rel="noreferrer" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" rel="noreferrer" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Eato App </h1>
+			<div className="card">
+				<Button />
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
 }
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-
 const Button = () => {
-  const sumOfTwo = (num1: number, num2: number): void => {
-    alert(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
-  };
+	const sumOfTwo = (num1: number, num2: number): void => {
+		alert(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
+	};
 
-  return (
-    <button onClick={() => sumOfTwo(5, 4)}>
-      Click me
-    </button>
-  );
+	return <button onClick={() => sumOfTwo(5, 4)}>Click me</button>;
 };
 
 export default Button;
