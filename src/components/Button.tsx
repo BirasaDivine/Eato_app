013e4eebb4e053cc72172e5b8963b91069ac32b6
Birasa Divine <b.divine@alustudent.com>
2025-05-09 14:35:18 +0200

    feature(button): add sumOfTwo function with alert (#1)
    * feature(button): add sumOfTwo function with alert
    
    * fix(Button):ES6 syntax on sumOfTwo and added export default on the button
    
    * fix(Button):ES6 syntax on sumOfTwo and added export default on the button
    
    * fix(Button):Intergrated prettier and ESlinter
    
    * fix(Prettier):Add config file
    
    * fix(prettier):Adding data in the config file
    
    * fix(vercel):Fix React Import

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+	const sumOfTwo = (num1: number, num2: number): void => {
+		alert(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
+	};
+
+	return <button onClick={() => sumOfTwo(5, 4)}>Click me</button>;
+};
+
+export default Button;
